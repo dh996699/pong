ad9bfc628b03afeafe8aad6263642630943ff50c
David Huynh <dh99.66.99@gmail.com>
2019-07-18 14:14:46 -0700

    add up and down key

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,54 @@
+import Board from './Board';
+import Paddle from './Paddle';
+import { SVG_NS, KEYS } from "../settings";
+
 export default class Game {
+
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+    this.gameElement = document.getElementById(this.element);
+    this.board = new Board(this.width, this.height);
+    
+    this.paddleWidth = 8;
+    this.paddleHeight = 56;
+    this.boardGap = 10;
+
+
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight) / 2),
+      'yellow',
+      KEYS.a,
+      KEYS.z
+    );
+
+    this.player2 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      (this.width - (this.boardGap+ this.paddleWidth)),//update this line to be 10px from the right
+      ((this.height - this.paddleHeight) / 2),
+      'red',
+      KEYS.up,
+      KEYS.down
+    );
   }
 
   render() {
-		// More code goes here....
+    this.gameElement.innerHTML = ''; // clear the html before appending to fix a render bug 🐞
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+    this.board.render(svg);
+    this.player1.render(svg);
+    this.player2.render(svg);
   }
 }
